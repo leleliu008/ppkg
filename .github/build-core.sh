105753a334f06e6725fca1c390be28425a0acde6
leleliu008 <leleliu008@gmail.com>
2025-06-26 21:28:17 +0800

    optimized

--- .github/build-core.sh
+++ .github/build-core.sh
@@ -7,47 +7,77 @@ run() {
   eval "$@"
 }
 
+if [ -z "$1" ] || [ -z "$2" ] ; then
+    printf 'Usage: .github/build-core.sh <RELEASE-VERSION> <TARGET-PLATFORM-SPEC>\n' >&2
+    exit 1
+fi
+
+######################################################
+
+TARGET_PLATFORM_NAME="${2%%-*}"
+TARGET_PLATFORM_ARCH="${2##*-}"
+
+######################################################
+
 if [ -f cacert.pem ] ; then
     run export SSL_CERT_FILE="$PWD/cacert.pem"
 fi
 
+######################################################
+
 run ./ppkg setup --syspm
 run ./ppkg update
-run ./ppkg install uppm@0.15.4 --static
-run ./ppkg bundle  uppm@0.15.4 .tar.xz
+run ./ppkg install $2/uppm@0.15.4 --static
+run ./ppkg bundle  $2/uppm@0.15.4 .tar.xz
 
-install -d out/
+######################################################
 
-for f in wrappers/*.c elftools/*.c
-do
-    x="${f#*/}"
-    o="out/${x%.c}"
-    run cc -flto -Os -std=gnu99 -o "$o" "$f"
-    run strip "$o"
-done
+install -d bundle.d/
 
-run export SSL_CERT_FILE="$HOME/.ppkg/core/cacert.pem"
+if [ "$TARGET_PLATFORM_NAME" = macos ] ; then
+    TARGET_PLATFORM_XXXX="${2%-*}"
+    TARGET_PLATFORM_VERS="${TARGET_PLATFORM_XXXX##*-}"
 
-curl -LO https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/OTF/SourceCodePro-Light.otf
+    CC="$(xcrun --sdk macosx --find clang)"
+    SYSROOT="$(xcrun --sdk macosx --show-sdk-path)"
+    CFLAGS="-isysroot $SYSROOT -mmacosx-version-min=$TARGET_PLATFORM_VERS -arch $TARGET_PLATFORM_ARCH -Qunused-arguments"
 
-bsdtar vxf uppm*.tar.xz -C out --strip-components=1
+    for f in core/wrappers/*.c
+    do
+        x="${f#*/}"
+        o="bundle.d/${x%.c}"
+        run $CC $CFLAGS -std=c99 -Os -flto -o "$o" "$f"
+        run strip "$o"
+    done
+else
+    for f in core/wrappers/*.c core/elftools/*.c
+    do
+        x="${f#*/}"
+        o="bundle.d/${x%.c}"
+        run cc -std=gnu99 -Os -s -flto -static -o "$o" "$f"
+    done
+fi
 
-mv out/bin/uppm *.otf core/fonts.conf out/
+######################################################
 
-rm -rf out/bin/ out/share/ out/.ppkg/
+run curl -LO https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/OTF/SourceCodePro-Light.otf
 
-NATIVE_OS_KIND="$(uname -s | tr A-Z a-z)"
-NATIVE_OS_ARCH="$(uname -m)"
+######################################################
 
-case $NATIVE_OS_KIND in
-    linux)
-        DIRNAME="ppkg-core-$1-$NATIVE_OS_KIND-$NATIVE_OS_ARCH"
-        ;;
-    *)  NATIVE_OS_VERS="$(uname -r)"
-        NATIVE_OS_VERS="${NATIVE_OS_VERS%%-*}"
-        DIRNAME="ppkg-core-$1-$NATIVE_OS_KIND-$NATIVE_OS_VERS-$NATIVE_OS_ARCH"
-esac
+if [ "$TARGET_PLATFORM_NAME" = linux ] ; then
+    BUNDLE_DIRNAME="ppkg-core-$1-$TARGET_PLATFORM_NAME-$TARGET_PLATFORM_ARCH"
+else
+    BUNDLE_DIRNAME="ppkg-core-$1-$2"
+fi
 
-mv out "$DIRNAME"
+######################################################
 
-bsdtar cavf "$DIRNAME.tar.xz" "$DIRNAME"
+run bsdtar vxf uppm*.tar.xz -C bundle.d --strip-components=1
+
+mv bundle.d/bin/uppm *.otf core/fonts.conf bundle.d/
+
+rm -rf bundle.d/bin/ bundle.d/share/ bundle.d/.ppkg/
+
+mv bundle.d "$BUNDLE_DIRNAME"
+
+run bsdtar cavf "$BUNDLE_DIRNAME.tar.xz" "$BUNDLE_DIRNAME"
