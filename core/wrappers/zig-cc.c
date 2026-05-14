2ca55e49d82a95888169bfe77d1127d774f95703
leleliu008 <leleliu008@gmail.com>
2026-05-14 09:27:04 +0800

    optimized

---
+++ core/wrappers/zig-cc.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+
+int main(int argc, char * argv[]) {
+    char * const zig = getenv("ZIG");
+
+    if (zig == NULL) {
+        fprintf(stderr, "ZIG environment variable is not set.\n");
+        return 1;
+    }
+
+    if (zig[0] == '\0') {
+        fprintf(stderr, "ZIG environment variable value should be a non-empty string.\n");
+        return 2;
+    }
+
+    const char* envs[] = { "CC", "CXX", "AS", "LD", NULL };
+
+    for (int i = 0; ; i++) {
+        if (envs[i] == NULL) {
+            break;
+        }
+
+        if (unsetenv(envs[i]) == -1) {
+            perror(NULL);
+            return 3;
+        }
+    }
+
+    char* args[argc + 3];
+
+    args[0] = zig;
+    args[1] = (char*)"cc";
+    args[argc+1] = NULL;
+
+    for (int i = 1; i < argc; i++) {
+        args[i+1] = argv[i];
+    }
+
+    execv (zig, args);
+    perror(zig);
+    return 255;
+}
---
+++ core/wrappers/zig-ranlib.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+
+int main(int argc, char * argv[]) {
+    char * const zig = getenv("ZIG");
+
+    if (zig == NULL) {
+        fprintf(stderr, "ZIG environment variable is not set.\n");
+        return 1;
+    }
+
+    if (zig[0] == '\0') {
+        fprintf(stderr, "ZIG environment variable value should be a non-empty string.\n");
+        return 2;
+    }
+
+    char* args[argc + 2];
+
+    args[0] = zig;
+    args[1] = (char*)"ranlib";
+    args[argc+1] = NULL;
+
+    for (int i = 1; i < argc; i++) {
+        args[i+1] = argv[i];
+    }
+
+    execv (zig, args);
+    perror(zig);
+    return 255;
+}
